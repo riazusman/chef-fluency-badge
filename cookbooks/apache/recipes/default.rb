814e63f8f48e8c2a4c98e7f82a19bdc34c3d88a4
Usman Riaz <riaz_usman@hotmail.com>
2017-12-10 19:47:29 +0000

    my revert

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,11 +5,11 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-if node ['platform_family'] == "rhel"
-	package = "httpd"
-elseif node ['platform_family'] == 'debian'
-	package = "apache2"
-end
+#if node ['platform_family'] == "rhel"
+#	package = "httpd"
+#elseif node ['platform_family'] == 'debian'
+#	package = "apache2"
+#end
 
 package 'apche2' do
 	package_name package
